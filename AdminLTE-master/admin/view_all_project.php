9651f8e1087f70aef00265c5b03a42cc68c35068
Husnee1221 <34243008+Husnee1221@users.noreply.github.com>
2020-04-15 21:48:41 +0700

    logo

--- AdminLTE-master/admin/view_all_project.php
+++ AdminLTE-master/admin/view_all_project.php
@@ -152,11 +152,10 @@ $count=mysqli_num_rows($result);
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">ITPROMOT</span>
-    </a>
+       <a href="index.php" class="brand-link">
+        <img src="../dist/img/n2.png" width="100%" >
+        <span class="brand-text font-weight-light"></span>
+      </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
